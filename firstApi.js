81a73f8ef65882cdb537250abd91aa7f42501d34
Prakhar-Bhalla <prakharbhalla2016@gmail.com>
2021-11-30 14:37:55 +0530

    first assignment

---
+++ firstApi.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const users = require("./users.json");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Home page");
+});
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+app.listen(2500, function() {
+    console.log("listening port 2500");
+});
